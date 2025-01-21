44747902cb91f6c3e85eeb8668b32f850ce4e817
aarshsingh <156768705+aarshsingh@users.noreply.github.com>
2025-01-21 17:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Tech VJ'
 
+@app.route("/health")
+def health_check():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)  # Ensure it listens on port 8080
